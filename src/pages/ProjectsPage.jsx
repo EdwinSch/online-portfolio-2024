b8c24f30deaca3925a03315547d5add7b49c787a
EdwinSch <edwinschutjes@gmail.com>
2024-03-27 13:40:32 +0100

    fix bugs

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import { projects } from "../data";
 import SingleProject from "../components/SingleProject/SingleProject";
+import { FaChevronLeft } from "react-icons/fa6";
 
 const ProjectsPage = () => {
+  // Window start at top
+  window.scrollTo(0, 0);
+
   return (
     <main className="projects-page-container">
       <Link to={"/"} className="back-btn">
-        &lsaquo; Go back
+        <FaChevronLeft /> Go back
       </Link>
       <h2>All Featured Projects</h2>
 
